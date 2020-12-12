c2a3d65cd01da3032158b2c3ae6d1f00ecb98493
toby-harris <75886735+toby-harris@users.noreply.github.com>
2020-12-12 14:05:59 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+let hand = 0
+input.onButtonPressed(Button.A, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
